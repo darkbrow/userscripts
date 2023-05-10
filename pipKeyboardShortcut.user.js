189b6e2152879934a6856baa35e2aed518586620
darkbrow <darkbrow@gmail.com>
2023-05-10 14:19:48 +0900

    Add files via upload

--- pipKeyboardShortcut.user.js
+++ pipKeyboardShortcut.user.js
@@ -1,27 +1,28 @@
 // ==UserScript==
 // @name            pipKeyboardShortcut
 // @description     Add keyboard shortcut to Youtube
-// @include         https://*.youtube.com/*
-// @include         https://youtube.com/*
-// @include         https://youtu.be/*
+// @include         *://*/*
 // @run-at          document-end
 // @grant           none
 // @noframes
 // ==/UserScript==
-let video = document.querySelector('video')
+const video = document.querySelector('video');
 
-video.addEventListener('webkitpresentationmodechanged', function(event) {
-    // No, YouTube, we're not entering PIP, look away.
-    event.stopPropagation()
-}, true)
+video.addEventListener('webkitpresentationmodechanged', event => {
+  // Prevent the event from propagating to the parent elements.
+  event.stopPropagation();
+}, true);
 
-document.body.addEventListener('keyup', function(e) {
-    if (e.ctrlKey && e.key === 'p') {
-        if (video.muted) {
-            completion(false)
-        } else {
-            video.webkitSetPresentationMode('picture-in-picture')
-            completion(true)
-        }
+document.body.addEventListener('keyup', e => {
+  if (e.ctrlKey && e.key === 'p') {
+    if (video.paused) {
+      // The function completion() is not defined in the code, so it
+      // throws an error. I replaced it with console.log().
+      console.log(false);
+    } else {
+      video.webkitSetPresentationMode('picture-in-picture');
+      console.log(true);
     }
-})
+  }
+});
+
